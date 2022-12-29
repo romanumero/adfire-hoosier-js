d1d031c76942972d751bb371b71a85ec98241d11
romanumero <damon@kortx.io>
2022-12-29 10:54:37 -0500

    wip

--- server/api/fire.post.ts
+++ server/api/fire.post.ts
@@ -1,5 +1,5 @@
 import {run} from "~/server/helpers/xandrHelper";
 
-export default defineEventHandler( event => {
-    return  run()
+export default defineEventHandler( (event) => {
+    return run()
 })
