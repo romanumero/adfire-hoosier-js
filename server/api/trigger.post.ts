dde05e46670af19e4555db03220b0b3165ad60e1
romanumero <damon@kortx.io>
2023-01-09 11:45:31 -0500

    testing path to email contents

--- server/api/trigger.post.ts
+++ server/api/trigger.post.ts
@@ -297,7 +297,7 @@ export default defineEventHandler( async (event) => {
                     purse: game.jackpot,
                 }
 
-                const source = fs.readFileSync(path.join(path.resolve(), 'server', 'templates', 'email.handlebars'), 'utf-8')
+                const source = fs.readFileSync(path.join(path.resolve(), 'templates', 'email.handlebars'), 'utf-8')
                 console.log(`Email Contents: ${source}`)
 
                 if (sendEmail) {
